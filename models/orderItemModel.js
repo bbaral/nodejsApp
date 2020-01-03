926d24febdd5055b7bef8b51c901aaca849c0e59
bbaral <baralbikram@hotmail.com>
2020-01-02 23:22:39 -0500

    complete shop App CRUD operation using Nodejs and MySQL

---
+++ models/orderItemModel.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const orderItem = sequelize.define('orderItem', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    quantity: {
+        type: Sequelize.INTEGER
+    }
+});
+
+module.exports = orderItem;
